82176aef586354ee1fdbe566d006ea9417225312
andrii-nester-gl <andrii.nester@globallogic.com>
2018-06-12 11:51:25 +0300

    Bottom popup on Map screen to show short location info

--- TakeHomeTest/Core/Models/ScenicPhotoLocation.swift
+++ TakeHomeTest/Core/Models/ScenicPhotoLocation.swift
@@ -7,12 +7,32 @@
 //
 
 import Foundation
+import MapKit
 
-struct ScenicPhotoLocation {
+class ScenicPhotoLocation: NSObject {
 
     let name: String
     let latitude: Double
     let longitude: Double
     let notes: String?
+
+    init(name: String, latitude: Double, longitude: Double, notes: String? = nil) {
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.notes = notes
+    }
+
+}
+
+extension ScenicPhotoLocation: MKAnnotation {
+
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+
+    var title: String? {
+        return name
+    }
 
 }
--- TakeHomeTest/Modules/Map/MapViewController.swift
+++ TakeHomeTest/Modules/Map/MapViewController.swift
@@ -18,25 +18,63 @@ class MapViewController: UIViewController {
                 return
             }
             showScenicPhotoLocations()
-            moveToFetchedPhotoLocations()
         }
     }
 
+    /// Newly created by user but not yet saved annotation
+    private var inProgressAnnotation: MKAnnotation?
+
+    private var selectedAnnotation: MKAnnotation? {
+        return mapView.annotations.map({ mapView.view(for: $0) }).compactMap({ $0 }).first(where: { $0.isSelected })?.annotation
+    }
+
+    private var isSelectedLocationSaved: Bool {
+        return selectedAnnotation as? ScenicPhotoLocation != nil
+    }
+
     @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var selectedLocationContainerView: UIStackView!
+    @IBOutlet private weak var selectedLocationInfoView: UIView!
+    @IBOutlet private weak var selectedLocationActionsView: UIView!
+    @IBOutlet private weak var selectedLocationTitleLabel: UILabel!
+    @IBOutlet private weak var selectedLocationSubtitleLabel: UILabel!
+    @IBOutlet weak var locationActionButton: UIButton!
 
     private let locationManager: CLLocationManager = CLLocationManager()
-    private let locationSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+
+    private let defaultAnimationDuration = 0.2
+
+    // MARK: - View lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        applyShadowToLocationInfoView()
         setupCurrentLocationTracking()
+        showScenicPhotoLocations()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        showScenicPhotoLocations()
-        moveToFetchedPhotoLocations()
     }
+
+    // MARK: - Actions
+
+    @IBAction func longPressGesture(_ sender: UILongPressGestureRecognizer) {
+        guard sender.state == .began else {
+            return
+        }
+        let touchPoint = sender.location(in: mapView)
+        let coordinate = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+        addAnnotation(
+            at: coordinate,
+            withName: NSLocalizedString("New location", comment: "Title for not yet saved locations")
+        )
+    }
+
+    @IBAction func locationActionButtonPressed(_ sender: Any) {
+    }
+
+    // MARK: - Private
 
     private func setupCurrentLocationTracking() {
         locationManager.delegate = self
@@ -53,36 +91,131 @@ class MapViewController: UIViewController {
         }
 
         mapView.removeAnnotations(mapView.annotations)
-
-        let markers = (viewModel.defaultLocations + viewModel.userLocations).map { location -> MKAnnotation in
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
-            annotation.title = location.name
-            return annotation
-        }
-        mapView.addAnnotations(markers)
+        let allAnnotations = viewModel.defaultLocations + viewModel.userLocations
+        mapView.addAnnotations(allAnnotations)
+        mapView.showAnnotations(allAnnotations, animated: true)
     }
 
-    private func moveToFetchedPhotoLocations() {
-        guard viewModel.hasLocations else {
-            return
+    private func addAnnotation(at coordinate: CLLocationCoordinate2D, withName name: String?) {
+        if let inProgressAnnotation = inProgressAnnotation {
+            mapView.removeAnnotation(inProgressAnnotation)
         }
-        guard let firstFetchedLocation = viewModel.defaultLocations.first ?? viewModel.userLocations.first else {
-            return
-        }
-        moveToLocation(CLLocationCoordinate2D(latitude: firstFetchedLocation.latitude, longitude: firstFetchedLocation.longitude))
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = name
+        inProgressAnnotation = annotation
+        mapView.addAnnotation(annotation)
     }
 
-    private func moveToLocation(_ location: CLLocationCoordinate2D) {
-        let region = MKCoordinateRegion(center: location, span: locationSpan)
-        mapView.setRegion(region, animated: true)
+    // MARK: - Helpers
+
+    private var hasSelectedAnnotations: Bool {
+        return mapView.annotations.map({ mapView.view(for: $0) }).compactMap({ $0 }).reduce(false, { $0 || $1.isSelected })
+    }
+
+    private func deselectAllAnnotations(except excludeAnnotation: MKAnnotation? = nil) {
+        mapView.annotations.map { annotation -> MKAnnotationView? in
+            guard annotation.isEqual(excludeAnnotation) == false else {
+                return nil
+            }
+            return mapView.view(for: annotation)
+            }.compactMap({ $0 }).forEach {
+                $0.setSelected(false, animated: true)
+        }
+    }
+
+    private func setSelectedLocationInfoView(hidden: Bool) {
+        UIView.animate(withDuration: defaultAnimationDuration) {
+            self.selectedLocationInfoView.isHidden = hidden
+            self.selectedLocationContainerView.layoutIfNeeded()
+        }
+    }
+
+    private func setSelectedLocationActionsView(hidden: Bool) {
+        UIView.animate(withDuration: defaultAnimationDuration) {
+            self.selectedLocationActionsView.isHidden = hidden
+            self.selectedLocationContainerView.layoutIfNeeded()
+        }
+    }
+
+    private func applyShadowToLocationInfoView() {
+        selectedLocationInfoView.layer.masksToBounds = false
+        selectedLocationInfoView.layer.shadowOffset = CGSize(width: 0, height: -2)
+        selectedLocationInfoView.layer.shadowOpacity = 0.3
+        selectedLocationInfoView.layer.shadowRadius = 2
+        selectedLocationInfoView.layer.shadowColor = UIColor.black.cgColor
+    }
+
+    private func setupSelectedLocationInfoView() {
+        guard let location = selectedAnnotation as? ScenicPhotoLocation else {
+            setupSelectedNewLocationInfoView()
+            return
+        }
+        selectedLocationTitleLabel.text = location.name
+
+        // TODO: get distance
+        selectedLocationSubtitleLabel.text = "distance"
+
+        UIView.animate(withDuration: defaultAnimationDuration) {
+            self.selectedLocationInfoView.isHidden = false
+            self.selectedLocationActionsView.isHidden = false
+            self.selectedLocationContainerView.layoutIfNeeded()
+        }
+    }
+
+    private func setupSelectedNewLocationInfoView() {
+        guard let newLocation = selectedAnnotation as? MKPointAnnotation else {
+            return
+        }
+        selectedLocationTitleLabel.text = newLocation.title
+
+        // TODO: get distance
+        selectedLocationSubtitleLabel.text = "distance"
+
+        UIView.animate(withDuration: defaultAnimationDuration) {
+            self.selectedLocationInfoView.isHidden = false
+            self.selectedLocationActionsView.isHidden = false
+            self.selectedLocationContainerView.layoutIfNeeded()
+        }
     }
 
 }
 
-extension MapViewController: CLLocationManagerDelegate {
+// MARK: - MKMapViewDelegate
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+extension MapViewController: MKMapViewDelegate {
+
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        guard let annotationView = views.last else {
+            return
+        }
+        guard let annotation = annotationView.annotation,
+            let inProgressAnnotation = inProgressAnnotation,
+            annotation.isEqual(inProgressAnnotation) else {
+                return
+        }
+        deselectAllAnnotations()
+        annotationView.setSelected(true, animated: true)
+        setupSelectedNewLocationInfoView()
     }
 
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        deselectAllAnnotations(except: view.annotation)
+        setupSelectedLocationInfoView()
+    }
+
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        if !hasSelectedAnnotations {
+            setSelectedLocationInfoView(hidden: true)
+            setSelectedLocationActionsView(hidden: true)
+        }
+    }
+
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    }
 }
